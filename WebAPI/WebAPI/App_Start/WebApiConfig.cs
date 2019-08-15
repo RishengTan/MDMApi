0405c345ee6a94f4b1bdcd5c22d23751d77aac84
Risheng Tan <rtan2@montefiore.org>
2019-08-15 14:08:00 -0400

    search LastName

---
+++ WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Net.Http.Headers;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        {
+            #region SetRequestResponseAlwaysToJsonFormat
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+            #endregion
+        }
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Formatters.Add(new CustomJsonFormatter()); //registering SetRequestResponseAlwaysToJsonFormat region
+            config.EnableCors();
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/GoldenRecordController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using MDMDataAccess;
+
+namespace WebAPI.Controllers
+{
+    public class GoldenRecordController : ApiController
+    {
+        [HttpGet]
+        [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+        public IEnumerable<vw_MDMGoldenRecord> GetAllRecords()
+        {
+            using (PVDR_MDMEntities entities = new PVDR_MDMEntities())
+            {
+                return entities.vw_MDMGoldenRecord.ToList();
+            }
+        }
+        [HttpGet]
+        [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+        public HttpResponseMessage GetRecordByNPI(string NPI)
+        {
+            using (PVDR_MDMEntities entities = new PVDR_MDMEntities())
+            {
+                if (NPI == null)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "NPI is empty");
+                }
+                else
+                {
+                    var searchResponse = entities.vw_MDMGoldenRecord.Where(e => e.NPI.ToLower() == NPI).ToList();
+                    if (searchResponse.Count() > 0)
+                        return Request.CreateResponse(HttpStatusCode.OK, searchResponse);
+                    else
+                        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "The NPI " + NPI + " not found in database");
+                }
+
+            }
+        }
+
+        [HttpGet]
+        [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+        public HttpResponseMessage searchRecordByNPI(string LastName)
+        {
+            using (PVDR_MDMEntities entities = new PVDR_MDMEntities())
+            {
+                if (LastName == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, "".ToArray());
+                }
+                else
+                {
+                    var searchResponse = entities.vw_MDMGoldenRecord.Where(e => e.LastName.Contains(LastName)).ToList();
+                    if (searchResponse.Count() > 0)
+                        return Request.CreateResponse(HttpStatusCode.OK, searchResponse);
+                    else
+                        return Request.CreateResponse(HttpStatusCode.OK,"".ToArray());
+                }
+
+            }
+        }
+    }
+}
